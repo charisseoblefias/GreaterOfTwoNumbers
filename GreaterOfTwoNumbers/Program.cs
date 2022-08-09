84868f34ee535d74cc3682932375b5cf7df374cc
char-kyeopta <charisse.oblefias01@gmail.com>
2022-08-09 12:09:18 +0800

    Add project files.

---
+++ GreaterOfTwoNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class GreaterOfTwoNumbers
+{
+	static void Main(String [] args)
+	{
+		Console.Write ("Enter the first number: ");
+		int a = Int32.Parse (Console.ReadLine());
+		
+		Console.Write ("Enter second Number: ");
+		int b = Int32.Parse (Console.ReadLine());
+		
+		int max = (a > b) ? a : b;
+		
+		Console.Write("The greater number is ");
+		Console.WriteLine(max);
+		Console.ReadKey();
+	}
+}
